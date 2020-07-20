8fabee98df383bb8149dccec26a3773f7e0b2e7d
Wan Sheng <wsheng1988@gmail.com>
2020-07-19 19:53:12 -0700

    deployment commit

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -11,68 +11,78 @@ import TextContainer from '../TextContainer/TextContainer';
 let socket;
 
 const Chat = ({ location }) => {
-  const [ name, setName ] = useState('');
-  const [ room, setRoom ] = useState('');
-  const [ users, setUsers ] = useState('');
-  const [ message, setMessage ] = useState('');
-  const [ messages, setMessages ] = useState([]);
-  // const ENDPOINT = 'localhost:8000';
+	const [name, setName] = useState('');
+	const [room, setRoom] = useState('');
+	const [users, setUsers] = useState('');
+	const [message, setMessage] = useState('');
+	const [messages, setMessages] = useState([]);
+	const ENDPOINT = 'localhost:8000';
 
+	useEffect(() => {
+		const { name, room } = queryString.parse(location.search);
 
-  useEffect(() => {
-    const { name, room } = queryString.parse(location.search);
+		socket = io(ENDPOINT);
 
-    socket = io(ENDPOINT);
+		setRoom(room);
+		setName(name);
 
-    setRoom(room);
-    setName(name)
+		socket.emit(
+			'join',
+			{
+				name,
+				room,
+			},
+			(error) => {
+				if (error) {
+					alert(error);
+				}
+			}
+		);
+	}, [ENDPOINT, location.search]);
 
-    socket.emit('join', { name, room }, (error) => {
-      if(error) {
-        alert(error);
-      }
-    });
-  }, [ENDPOINT, location.search]);
+	useEffect(() => {
+		// handle message
+		socket.on('message', (message) => {
+			// listen for message event
+			setMessages([...messages, message]); // take the message send by admin or otehrs, will be set into the messages state
+		});
 
+		socket.on('roomData', ({ users }) => {
+			setUsers(users);
+		});
 
-  useEffect(() => {  // handle message
-    socket.on('message', (message) => {      // listen for message event
-      setMessages([...messages, message ]);  // take the message send by admin or otehrs, will be set into the messages state
-    });
+		return () => {
+			socket.emit('disconnect');
 
-    socket.on('roomData', ({ users }) => {
-      setUsers(users);
-    })
+			socket.off();
+		};
+	}, [messages]); // reload on when messages change
 
-    return () => {
-      socket.emit('disconnect');
+	const sendMessage = (event) => {
+		// handle send message
+		event.preventDefault();
 
-      socket.off();
-    }
+		if (message) {
+			// if there is message
+			socket.emit('sendMessage', message, () => setMessage('')); // socket will emit the message to the backend - send the message back - then clear the message
+		}
+	};
 
-  }, [messages])  // reload on when messages change
+	// console.log(message, messages);
 
-
-  const sendMessage = (event) => { // handle send message
-    event.preventDefault();
-  
-    if(message) {  // if there is message
-      socket.emit('sendMessage', message, () => setMessage(''));  // socket will emit the message to the backend - send the message back - then clear the message
-    }
-  }
-
-  // console.log(message, messages);
-
-  return (
-    <div className="outerContainer">
-      <div className="container">
-        <InfoBar room={room} />
-        <Messages messages={messages} name={name} />
-        <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
-      </div>
-      <TextContainer users={users}/>
-    </div>
-  );
-}
+	return (
+		<div className='outerContainer'>
+			<div className='container'>
+				<InfoBar room={room} /> <Messages messages={messages} name={name} />{' '}
+				<Input
+					message={message}
+					setMessage={setMessage}
+					sendMessage={sendMessage}
+				/>{' '}
+			</div>{' '}
+			<TextContainer users={users} />{' '}
+		</div>
+	);
+};
 
 export default Chat;
